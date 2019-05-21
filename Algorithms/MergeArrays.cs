b739af5a113bdccf29433d5cd0d673cfe380f8bf
abhinav katiyar <abhinav.katiyar@live.com>
2019-05-21 17:54:03 +0530

    Merge two arrays done

---
+++ Algorithms/MergeArrays.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class MergeArrays
+    {
+        public int[] Merge(int[] ar1,int[] ar2)
+        {
+            int ar1Pointer = 0, ar2Pointer = 0, targetIndex = 0;
+            int remaining = ar1.Length + ar2.Length;
+            int[] mergedArray = new int[remaining];
+            
+           while(remaining > 0)
+            {
+                if (ar1Pointer >= ar1.Length) //handling if the end of first array, copy the remaining elemnt of array 2
+                {
+                    mergedArray[targetIndex] = ar2[ar2Pointer++];                   
+                }else if(ar2Pointer >= ar2.Length)
+                {
+                    mergedArray[targetIndex] = ar1[ar1Pointer++];
+                   
+                }else if(ar1[ar1Pointer] < ar2[ar2Pointer])
+                {
+                    mergedArray[targetIndex] = ar1[ar1Pointer++];                    
+                }else if(ar1[ar1Pointer] == ar2[ar2Pointer])
+                {
+                    mergedArray[targetIndex++] = ar2[ar2Pointer];                  
+                    mergedArray[targetIndex] = ar2[ar2Pointer];
+                    ar2Pointer++;ar1Pointer++;
+                }
+                else
+                {
+                    mergedArray[targetIndex] = ar2[ar2Pointer++];                    
+                }
+
+                targetIndex++;
+                remaining--;
+            }
+            return mergedArray;
+        }
+    }
+}
--- ConsoleAppRunner/Program.cs
+++ ConsoleAppRunner/Program.cs
@@ -11,8 +11,16 @@ namespace ConsoleAppRunner
     {
         static void Main(string[] args)
         {
+            int[] ar1 = new int[4] { 2, 4, 20, 90 };
+            int[] ar2 = new int[5] { 1, 3, 5,6,10 };
+            MergeArrays mergeArrays = new MergeArrays();
+            mergeArrays.Merge(ar1, ar2);
+        }
+
+        private static void Sudoku()
+        {
             int[,] sudoku_data_valid = new int[,]
-            {
+                        {
                 {5, 3, 4, 6, 7, 8, 9, 1, 2 },
                 { 6, 7, 2, 1, 9, 5, 3, 4, 8 },
                 { 1, 9, 8, 3, 4, 2, 5, 6, 7},
@@ -22,10 +30,9 @@ namespace ConsoleAppRunner
                 { 9, 6, 1, 5, 3, 7, 2, 8, 4 },
                 { 2, 8, 7, 4, 1, 9, 6, 3, 5 },
                 { 3, 4, 5, 0, 8, 6, 1, 0, 0 }
-            };
-           Sudoku sudoku = new Sudoku();
-           var result = sudoku.Validate(sudoku_data_valid);
-
+                        };
+            Sudoku sudoku = new Sudoku();
+            var result = sudoku.Validate(sudoku_data_valid);
         }
     }
 }
