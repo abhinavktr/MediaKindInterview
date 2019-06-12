e5462be5b75fd0190733b272e6a8ecac126aee29
abhinav katiyar <abhinav.katiyar@live.com>
2019-06-12 17:35:14 +0530

    adding quick sort

--- Algorithms/QuickSort.cs
+++ Algorithms/QuickSort.cs
@@ -6,8 +6,49 @@ using System.Threading.Tasks;
 
 namespace Algorithms
 {
-    class QuickSort
+   public class QuickSort
     {
+        public static void Sort(int[] input, int low, int high)
+        {            
+            if(low < high)
+            {
+                int pi = partition(input, low, high);
+                Sort(input, low, pi - 1);
+                Sort(input, pi + 1, high);
+            }
+        }
 
+        private static int partition(int[] input, int low, int high)
+        {
+            int pivot = input[high];
+            // index of smaller element 
+            int i = (low - 1);
+            for (int j = low; j < high; j++)
+            {
+                // If current element is smaller  
+                // than or equal to pivot 
+                if (input[j] <= pivot)
+                {
+                    i++;
+
+                    // swap arr[i] and arr[j] 
+                    int temp = input[i];
+                    input[i] = input[j];
+                    input[j] = temp;
+                }
+            }
+            // swap arr[i+1] and arr[high] (or pivot) 
+            int temp1 = input[i + 1];
+            input[i + 1] = input[high];
+            input[high] = temp1;
+            return i + 1;
+        }
+
+        private static void Swap(int[] input, int index1, int index2)
+        {
+            var temp = input[index2];
+            input[index2] = input[index1];
+            input[index1] = temp;           
+        }
     }
 }
